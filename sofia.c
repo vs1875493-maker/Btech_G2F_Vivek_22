cb34c75e369dc5d4f247c36561251e90c6cb9002
vs1875493-maker <vs1875493@gmail.com>
2026-02-10 11:37:48 +0530

    Add files via upload

---
+++ sofia.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter first number");
+    scanf("%d",&a);
+    printf("enter second number");
+    scanf("%d",&b);
+
+
+    if (a == b){
+        printf("both number are eqaul:");
+
+    } else {
+        printf("both number are no equal:");
+
+
+    }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
